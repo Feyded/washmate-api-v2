779d954fff80979a59bf7cfc3373e3ea9e8ed336
Dean Zaballero <126765808+Feyded@users.noreply.github.com>
2026-08-20 14:58:53 +0800

    feat: order item addons

---
+++ app/Http/Controllers/OrderItemAddonController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OrderItemAddon;
+use Illuminate\Http\Request;
+
+class OrderItemAddonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OrderItemAddon $orderItemAddon)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(OrderItemAddon $orderItemAddon)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, OrderItemAddon $orderItemAddon)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OrderItemAddon $orderItemAddon)
+    {
+        //
+    }
+}
