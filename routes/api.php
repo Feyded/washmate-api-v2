bd8eab8e7a01d96311c110711450d8d7d49d002b
Dean Zaballero <126765808+Feyded@users.noreply.github.com>
2026-09-08 17:59:47 +0800

    chore: remove unused api routes

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,4 @@
 <?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
 require __DIR__ . '/api/admin.php';
 require __DIR__ . '/api/pos.php';
 require __DIR__ . '/api/auth.php';
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
